6e46b81aa016d647a464acd4d55acf0fe6714a3b
nyon3 <tomoya@crosstalk.me>
2020-10-07 01:32:30 +0900

    Add PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,18 +18,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `time-calculator`,
+        short_name: `calc`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ct_icon.jpg`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`
   ],
 }
